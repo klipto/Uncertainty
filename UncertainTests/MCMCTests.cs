698c7f48a620d039eba5a5c2fd14a67a5113012f
Todd Mytkowicz <toddm@microsoft.com>
2016-02-01 08:42:59 -0800

    Fixed bug in History operator and added HMM test code.

--- UncertainTests/MCMCTests.cs
+++ UncertainTests/MCMCTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Research.Uncertain;
 using Microsoft.Research.Uncertain.Inference;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace UncertainTests
 {
@@ -69,6 +70,70 @@ namespace UncertainTests
         }
 
         [TestMethod]
+        public void TestHMM2()
+        {
+            var states = new[] { "Healthy", "Fever" };
+
+            var emits = new[] { "normal", "cold", "dizzy" };
+
+            var source = new[] {
+                new Multinomial<string>(emits, new [] {0.8, 0.1, 0.1}),
+                new Multinomial<string>(emits, new [] {0.2, 0.7, 0.1}),
+                new Multinomial<string>(emits, new [] {0.05, 0.05, 0.9}),
+            };
+
+            var observations = source.History(3);
+
+            var start_probability = new Multinomial<string>(states, new[] { 0.6, 0.4 });
+
+            Func<string, Multinomial<string>> transition_probability = state =>
+            {
+                if (state == "Healthy")
+                    return new Multinomial<string>(states, new[] { 0.7, 0.3 });
+                if (state == "Fever")
+                    return new Multinomial<string>(states, new[] { 0.4, 0.6 });
+
+                throw new Exception("Unknown state");
+            };
+
+            Func<string, Multinomial<string>> emission_probability = state =>
+            {
+                if (state == "Healthy")
+                    return new Multinomial<string>(new[] { "normal", "cold", "dizzy" }, new[] { 0.5, 0.4, 0.1 });
+                if (state == "Fever")
+                    return new Multinomial<string>(new[] { "normal", "cold", "dizzy" }, new[] { 0.1, 0.3, 0.6 });
+
+                throw new Exception("Unknown state");
+            };
+
+            var program = from obs in observations
+
+                          from prior in start_probability
+
+                          from state0 in transition_probability(prior)
+                          from emit0 in emission_probability(state0)
+                          where obs[0] == emit0
+
+                          from state1 in transition_probability(state0)
+                          from emit1 in emission_probability(state1)
+                          where obs[1] == emit1
+
+                          from state2 in transition_probability(state1)
+                          from emit2 in emission_probability(state2)
+                          where obs[2] == emit2                          
+
+                          select new { state0, state1, state2 };
+
+            var output = program.Inference().Support().OrderByDescending(k => k.Probability).ToList();
+            var sampled = program.SampledInference(1000).Support().OrderByDescending(k => k.Probability).ToList();
+
+            Assert.AreEqual(output[0].Value, sampled[0].Value);
+            Assert.IsTrue(Math.Abs(output[0].Probability - sampled[0].Probability) < 0.1);
+
+            int x = 10;
+        }
+
+        [TestMethod]
         public void TestDan()
         {
             var r = new RandomMath();
@@ -77,7 +142,7 @@ namespace UncertainTests
             // is high or low
             Uncertain<bool>[] data = (from i in Enumerable.Range(0, N)
                                       let noise = r.NextGaussian(0, 0.01)
-                                      let vad = i > 15 && i < 30 ? 0.9 : 0.01
+                                      let vad = i > 15 && i < 30 ? 0.9 : 0.1
                                       let param = Math.Abs(vad + noise)
                                       let f = new Bernoulli(param > 1 ? 1 : param)
                                       select f).ToArray();
@@ -95,11 +160,31 @@ namespace UncertainTests
                 .Take(5) // just top 5
                 .ToList(); // produce list
 
+            //var program = from bools in history
+            //              let sum = bools.Select(Convert.ToInt32).Sum()
+            //              from prior in new Gaussian(20, 0.01)
+            //              where sum == (int) prior
+            //              select bools;
+            //Uncertain<bool[]> posterior1 = program.SampledInference(10000, new BoolArrayEqualityComparer());
+
+            Func<bool[], bool[]> Intervalize = _ => _;
+
+            var program = from bools in data.History(N)
+                          select Intervalize(bools);
+
+            // now inspect by materializing a list
+            List < Weighted < bool[] >> top51 = posterior
+                .Support() // enumerate the histogram                
+                .OrderByDescending(k => k.Probability) // sorted by probability
+                .Take(5) // just top 5
+                .ToList(); // produce list
+
+
             // set breakpoint
             int x = 10;
         }
     }
-    public class FunctionalList<T>
+    public class FunctionalList<T> : IEnumerable<T>
     {
         // Creates a new list that is empty
         public FunctionalList()
@@ -118,6 +203,16 @@ namespace UncertainTests
         // Properties valid for a non-empty list
         public T Head { get; private set; }
         public FunctionalList<T> Tail { get; private set; }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return FunctionalList.Helper<T>(this).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return FunctionalList.Helper<T>(this).GetEnumerator();
+        }
     }
 
     // Static class that provides nicer syntax for creating lists
@@ -133,7 +228,7 @@ namespace UncertainTests
             return new FunctionalList<T>(head, tail);
         }
 
-        private static IEnumerable<T> Helper<T>(FunctionalList<T> lst)
+        internal static IEnumerable<T> Helper<T>(FunctionalList<T> lst)
         {
             if (lst.IsEmpty) yield break;
             yield return lst.Head;
@@ -147,10 +242,11 @@ namespace UncertainTests
             return array;
         }
     }
+
     public static class MyExtensions
     {
         public static Uncertain<T[]> History<T>(this IEnumerable<Uncertain<T>> source, int num)
-        {
+        {           
             Uncertain<T[]> output = source.Take(num).Aggregate<Uncertain<T>, Uncertain<FunctionalList<T>>, Uncertain<T[]>>(
                 FunctionalList.Empty<T>(),
                 (i, j) =>
@@ -162,7 +258,8 @@ namespace UncertainTests
                 uncertainlst =>
                 {
                     return from sample in uncertainlst
-                           select FunctionalList.ToArray(sample);
+                           select sample.Reverse().ToArray();
+                           //FunctionalList.ToArray(sample);
                 });
             return output;
         }
