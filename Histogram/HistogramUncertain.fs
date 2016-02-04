5c04d9629e359d3e1c93b5dc84e86dd37f3122c7
Adrian Sampson <adrian@radbox.org>
2016-02-04 15:44:08 -0800

    Skeleton for histogram class

---
+++ Histogram/HistogramUncertain.fs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.Research.Uncertain.Histogram
+
+open Microsoft.Research.Uncertain
+
+type HistogramUncertain<'a>() =
+    inherit RandomPrimitive<'a>()
+    let entries = List.empty<Weighted<'a>>
+
+    override this.GetSupport () =
+        // TODO entries
+        seq {
+            yield entries.Head
+        }
+    
+    override this.GetSample () =
+        // TODO actually sample
+        entries.Head.Value
+
+    override this.StructuralEquals other =
+        // TODO
+        true
+
+    override this.GetStructuralHash () =
+        // TODO
+        0
+
+    override this.Score value =
+        // TODO
+        0.0
