1602a338411ff0ea508b7619c055fd4e6a41301e
Adrian Sampson <adrian@radbox.org>
2016-02-04 16:46:38 -0800

    Constructor for HistogramUncertain

--- Histogram/Example.fsx
+++ Histogram/Example.fsx
@@ -2,4 +2,4 @@
 #r "bin/Debug/Histogram.dll"
 open Microsoft.Research.Uncertain.Histogram
 
-let foo = new HistogramUncertain<float> ()
+let foo = new HistogramUncertain<float> ([ (73.0, 0.8); (42.1, 0.15) ])
--- Histogram/HistogramUncertain.fs
+++ Histogram/HistogramUncertain.fs
@@ -1,19 +1,30 @@
 ﻿namespace Microsoft.Research.Uncertain.Histogram
 open Microsoft.Research.Uncertain
 
-type HistogramUncertain<'a>() =
+type HistogramUncertain<'a> (topk: seq< 'a * float >) =
     inherit RandomPrimitive<'a>()
-    let entries = List.empty<Weighted<'a>>
+
+    // A list of object/probability pairs.
+    let entries =
+        List.ofSeq topk
+
+    // The "non-top" probability.
+    member this.otherProbability () =
+        let allProbs = seq { for value, probability in entries -> probability } in
+        let totalProb = Seq.fold (+) 0.0 allProbs in
+        let topkProb = 0.0 in
+        1.0 - topkProb
+    
+
+    // The Uncertain<T> interface.
 
     override this.GetSupport () =
-        // TODO entries
-        seq {
-            yield entries.Head
-        }
+        raise (System.Exception("infinite support"))
     
     override this.GetSample () =
         // TODO actually sample
-        entries.Head.Value
+        let value, probability = entries.Head in
+        value
 
     override this.StructuralEquals other =
         // TODO
