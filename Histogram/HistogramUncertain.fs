55acfdc4454380f2ca51cabc1982e4893fb9c0fe
Adrian Sampson <adrian@radbox.org>
2016-02-05 13:19:10 -0800

    Dead utilities module

--- Histogram/HistogramUncertain.fs
+++ Histogram/HistogramUncertain.fs
@@ -1,31 +1,14 @@
 ﻿namespace Microsoft.Research.Uncertain.Histogram
 open Microsoft.Research.Uncertain
 
-// Functional utilities for implementing HistogramUncertain.
-module Histogram =
-    let rec getScore entries value =
-        match entries with
-        | (v, p)::es -> (if v = value then p else getScore es value)
-        | _ -> 0.0
-
-    let rec sampleIndex index entries =
-        match entries with
-        | (v, p)::es -> (if index <= p
-                         then Some v
-                         else sampleIndex (index - p) entries)
-        | _ -> None
-
-
 // A convenient F# type alias for "partial distributions."
 type 'a unc = Uncertain<'a option>
-
 
 // Our combinator library.
 module public Lifting =
     // Propagate uncertainty through an 'a -> 'b.
     let lift (f: 'a -> 'b) (ua: 'a unc) : 'b unc =
         ua.Select(fun a -> Option.map f a)
-
 
 // A (partial) Uncertain<T> implementaiton wrapping a "top-K-plus-other"
 // histogram representation. It is based on the Multinomial primitive from
