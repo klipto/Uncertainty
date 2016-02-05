e81b72a89324292115336874e9b801fb8b61004c
Adrian Sampson <adrian@radbox.org>
2016-02-05 10:53:16 -0800

    Output the histogram

--- Histogram/Example.fsx
+++ Histogram/Example.fsx
@@ -3,3 +3,8 @@
 open Microsoft.Research.Uncertain.Histogram
 
 let foo = new HistogramUncertain<float> ([ (73.0, 0.8); (42.1, 0.15) ])
+
+for weighted in foo.Support() do
+    match weighted.Value with
+    | Some v -> printfn "%A: %f" v weighted.Probability
+    | None -> printfn "other: %f" weighted.Probability
