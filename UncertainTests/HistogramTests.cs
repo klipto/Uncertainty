08f9fcf813096774c70f607bdb585288c3015c1a
Adrian Sampson <adrian@radbox.org>
2016-02-05 14:06:51 -0800

    A skeleton for tests

---
+++ UncertainTests/HistogramTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Microsoft.Research.Uncertain;
+using Microsoft.Research.Uncertain.Histogram;
+
+namespace UncertainTests
+{
+    [TestClass]
+    public class HistogramTests
+    {
+        [TestMethod]
+        public void Flattening()
+        {
+            var someDist = new Multinomial<int>(1, 2, 2, 3, 3, 3, 4);
+
+            var someHist = Histogram.flatten(someDist);
+        }
+    }
+}
