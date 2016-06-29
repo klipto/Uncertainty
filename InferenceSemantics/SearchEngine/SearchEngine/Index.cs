d139e4137ff9aa2d494518fe086e911c9551f3c3
t-chnand <t-chnand@microsoft.com>
2016-06-29 09:59:57 -0700

    skeletons

---
+++ InferenceSemantics/SearchEngine/SearchEngine/Index.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    class Index
+    {
+        public void indexer(WebPage wegpage) {
+
+        }
+    }
+}
