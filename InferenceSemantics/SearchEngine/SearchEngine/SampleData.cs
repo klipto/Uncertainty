86e42fe90ba21a8c8f3a91379a19dd83d02ece63
t-chnand <t-chnand@microsoft.com>
2016-06-29 18:37:05 -0700

    code for a lucene based search engine, right now not giving any output for some reason

--- InferenceSemantics/SearchEngine/SearchEngine/SampleData.cs
+++ InferenceSemantics/SearchEngine/SearchEngine/SampleData.cs
@@ -6,11 +6,10 @@ using System.Threading.Tasks;
 
 namespace SearchEngine
 {
-    class PageRank
+   public class SampleData
     {
-        public void rank(WebPage webpage)
-        {
-
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
---
+++ InferenceSemantics/SearchEngine/SearchEngine/Search.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Version = Lucene.Net.Util.Version;
+
+namespace SearchEngine
+{
+    public class Search
+    {
+        private IndexSearcher searcher = null;
+        private QueryParser parser = null;
+
+        public Search()
+        {
+            searcher = new IndexSearcher(FSDirectory.Open(new DirectoryInfo("index-directory")), false);
+            parser = new QueryParser(Version.LUCENE_30,"content", new StandardAnalyzer(Version.LUCENE_30));
+        }
+        public TopDocs performSearch(String query, int n)
+        {
+            Query q = parser.Parse(query);
+            return searcher.Search(q, n);
+        }
+        public Document getDocument(int docID)
+        {
+            return searcher.Doc(docID);
+        }
+    }
+}
