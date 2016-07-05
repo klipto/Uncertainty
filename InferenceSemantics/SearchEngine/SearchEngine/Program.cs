1bf509639555c37c0b55754f424bcf6059d77118
t-chnand <t-chnand@microsoft.com>
2016-07-05 11:28:56 -0700

    updates

--- InferenceSemantics/SearchEngine/SearchEngine/Program.cs
+++ InferenceSemantics/SearchEngine/SearchEngine/Program.cs
@@ -17,26 +17,26 @@ namespace SearchEngine
 {
     class Program
     {
-        public static int number_of_machines= 3;   
-        public static Dictionary<int, List<SampleData>> data_partitions_for_distributed_search = 
+        public static int number_of_machines = 3;
+        public static Dictionary<int, List<SampleData>> data_partitions_for_distributed_search =
             new Dictionary<int, List<SampleData>>();
 
-        static Dictionary<int, List<SampleData>> CreateDataPartitions (List<SampleData> dataset, int number_of_machines)
+        static Dictionary<int, List<SampleData>> CreateDataPartitions(List<SampleData> dataset, int number_of_machines)
         {
             Dictionary<int, List<SampleData>> partitions = new Dictionary<int, List<SampleData>>();
             Dictionary<int, int> partition_sizes = new Dictionary<int, int>();
-            int partition_size=0;
+            int partition_size = 0;
 
             // assuming all servers are equally powerful, we try to allocate the same number of searches to each of them. 
             partition_size = dataset.Count / number_of_machines;
             for (int x = 1; x <= number_of_machines; x++)
             {
                 partition_sizes.Add(x, partition_size);
-            }                    
+            }
 
             // if the dataset cannot be split equally, distribute the extras among the machines as evenly as possible.
             if (dataset.Count % number_of_machines != 0)
-            {    
+            {
                 int extras = dataset.Count % number_of_machines;
                 for (int x = 1; x <= extras; x++)
                 {
@@ -46,53 +46,70 @@ namespace SearchEngine
 
             int index = 0;
             foreach (var machine_partition_size in partition_sizes)
-            {      
-                List<SampleData> partition = new List<SampleData>();     
+            {
+                List<SampleData> partition = new List<SampleData>();
                 for (int x = index; x < index + machine_partition_size.Value; x++)
                 {
                     partition.Add(dataset[x]);
                 }
                 index = index + machine_partition_size.Value;
-                partitions.Add(machine_partition_size.Key, partition);                
-            }        
+                partitions.Add(machine_partition_size.Key, partition);
+            }
             return partitions;
         }
 
-        public static void finalSearch()
+        public static void finalSearch(Dictionary<int, Dictionary<Field, double>> score_summaries)
         {
-
+            foreach (var key in score_summaries.Keys)
+            {
+                foreach (var key1 in score_summaries[key].Keys)
+                {
+                    Console.Write(key1 + " : " + score_summaries[key][key1] + "\n");
+                }
+            }
         }
+
         static void Main(string[] args)
         {
-           try
+            Dictionary<int, Dictionary<Field, double>> score_summaries = new Dictionary<int, Dictionary<Field, double>>();
+            try
             {
                 data_partitions_for_distributed_search = CreateDataPartitions(SampleDataRepository.GetAll(), number_of_machines);
-                int machine=1;
+                int machine = 1;
 
                 // distribute search to available servers
                 foreach (var data_partition in data_partitions_for_distributed_search)
                 {
+                    Dictionary<Field, double> score_ratios = new Dictionary<Field, double>();
+
                     Console.Write("Machine " + machine + " building indexes\n");
                     Index indexer = new Index();
                     indexer.rebuildIndex(data_partition.Value);
                     Console.Write("Building indexes done\n");
-                    Console.Write("Machine "+ machine + " performing search\n");
+                    Console.Write("Machine " + machine + " performing search\n");
                     Search s = new Search();
+
+
                     TopDocs topDocs = s.performSearch("Allahabad Seattle", 5);
                     Console.Write("Results found: " + topDocs.TotalHits + "\n");
+
                     ScoreDoc[] hits = topDocs.ScoreDocs;
                     for (int x = 0; x < hits.Length; x++)
                     {
                         Document doc = s.getDocument(hits[x].Doc);
+                        double score_ratio = hits[x].Score / topDocs.MaxScore;
                         Console.Write(doc.GetField("Id") + " " + doc.GetField("Name") + " " + doc.GetField("Description") + " " + hits[x].Score);
                         Console.Write("\n");
+                        score_ratios.Add(doc.GetField("Id"), score_ratio);
                     }
+                    score_summaries.Add(machine, score_ratios);
                     machine++;
                     Console.Write("Finished\n");
+
                 }
-               
-               // final search in the "central server" using results from the other servers
-                finalSearch();
+
+                // final search in the "central server" using results from the other servers
+                finalSearch(score_summaries);
             }
             catch (Exception e)
             {
