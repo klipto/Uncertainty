753e206f5b87911ee1bc6c5ad4d85fdd9f73d012
cse <cse>
2016-10-06 20:25:52 -0700

    update

--- InferenceSemantics/ComplexInferenceBenchmark/ComplexInferenceBenchmark/Program.cs
+++ InferenceSemantics/ComplexInferenceBenchmark/ComplexInferenceBenchmark/Program.cs
@@ -14,20 +14,43 @@ namespace ComplexInferenceBenchmark
 	class MainClass
 	{
 		public static double Inference1(Uncertain<double>program1, Uncertain<double> program2, Uncertain<double> program3) {
-			var exp_enum = program2.SampledInference(1000).Support().ToList();
-			List<double> final_sample_values = new List<double> ();
-			var g1 = program1.SampledInference (1000).Support ().ToList ();
-			foreach (var g in g1) {
-				foreach (var e in exp_enum) {
-					final_sample_values.Add (g.Value + e.Value);
-				}
+            var g1 = program1.SampledInference(100).Support().ToList();
+            var g2 = program2.SampledInference(100).Support().ToList();
+            var exp_enum = program3.SampledInference(100).Support().ToList();
+            
+        
+			List<Tuple<double, double>> sample_values = new List<Tuple<double,double>> ();			
+			foreach (var g in g1) 
+            {
+                foreach (var h in g2)
+                {
+                    sample_values.Add(Tuple.Create(g.Value + h.Value, g.Probability*h.Probability));  // these values are N(0,2)               
+                }
 			}
+            List<Tuple<double, double>> sample_values1 = new List<Tuple<double, double>>();
+            foreach (var e in exp_enum)
+            {
+                foreach (var f in sample_values)
+                {
+                    sample_values1.Add(Tuple.Create((e.Value + f.Item1),e.Probability*f.Item2));
+                }
+            }
+
+            List<Tuple<double, double>> sample_values2 = new List<Tuple<double, double>>();
+            foreach (var g in g1)
+            {
+                foreach (var s in sample_values1)
+                {
+                    sample_values2.Add(Tuple.Create(g.Value + s.Item1, g.Probability*s.Item2));
+                }
+            }
+
 			double sum = 0.0;
-			foreach (var v in final_sample_values) {
-				sum = sum + v;
+			foreach (var v in sample_values2) {
+				sum = sum + v.Item1;
 
 			}
-			double mean1 = sum / final_sample_values.Count;
+			double mean1 = sum / sample_values1.Count;
 			return mean1;
 		}
 
@@ -41,7 +64,7 @@ namespace ComplexInferenceBenchmark
 			//			   from p3 in program3.SampledInference (1000, null)
 			//		select (p + p3);
 
-			var final_program = program.SampledInference (1000).Support ().ToList ();
+			var final_program = program.SampledInference(1000).Support ().ToList ();
 			double sum = 0.0;
 			foreach (var v in final_program) {
 				sum = sum + v.Value;
@@ -53,16 +76,13 @@ namespace ComplexInferenceBenchmark
 		public static void Main (string[] args)
 		{
 			var program1 = new Gaussian (0, 1);
-			var program2 = new Exponential (5);
-			var program3 = new Uniform<double> (0,1);
+			var program2 = new Gaussian (0, 1);
+			var program3 = new Exponential(5);
 
 			var t1= Inference1(program1, program2, program3);
 			System.Console.WriteLine("mean1: " + t1);
 			var t2=Inference2(program1, program2, program3);			
 			System.Console.WriteLine(" mean2: " + t2);	
-
 		}
-
-
 	}
 }
